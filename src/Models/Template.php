ae3999acc7e5d5ec54cf7897b71d42b72f840da5
railken <railken@protonmail.ch>
2019-03-19 17:18:04 +0100

    update relations

--- src/Models/Template.php
+++ src/Models/Template.php
@@ -3,6 +3,8 @@
 namespace Railken\Amethyst\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Railken\Amethyst\Common\ConfigurableModel;
 use Railken\Amethyst\Managers\TemplateManager;
@@ -46,7 +48,7 @@ class Template extends Model implements EntityContract
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function data_builder()
+    public function data_builder(): BelongsTo
     {
         return $this->belongsTo(DataBuilder::class);
     }
